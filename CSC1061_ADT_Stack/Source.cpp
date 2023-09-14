8118ebbb97cbcf8124a07540ac76b4d917673f30
Kathleen Geise <kmgeise2@gmail.com>
2023-09-14 13:39:56 -0600

    Add project files.

---
+++ CSC1061_ADT_Stack/Source.cpp
@@ -0,0 +1,15 @@
+// CSC1061_ADT_Stack
+// This example is a stack using an ADT
+//Program to test the various operations of a stack
+
+#include <iostream>
+#include "myStack.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Example 18-1 shows how a stack is used." << endl;
+
+	return 0;
+}
